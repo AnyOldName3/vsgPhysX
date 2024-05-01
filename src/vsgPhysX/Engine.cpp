d2ca125e0eb82b135864950ab1c3d8c3b4fecb9c
AnyOldName3 <krizdjali@gmail.com>
2024-05-01 15:42:02 +0100

    Implement log adapter

--- src/vsgPhysX/Engine.cpp
+++ src/vsgPhysX/Engine.cpp
@@ -7,13 +7,15 @@
 #include <foundation/PxFoundation.h>
 #include <foundation/PxPhysicsVersion.h>
 
+#include <vsg/io/Logger.h>
+
 #include <iostream>
 #include <stdexcept>
 
 namespace
 {
-    // TODO: Make these configurable, or at least VSG-based.
-    // I guess it's sensible to unconditionally wire logging to VSG's logging and adapt VSG's allocator with a PhysX-specific affinity as the default PhysX allocator doesn't have any anti-fragmentation measures.
+    // TODO: Make this configurable, or at least VSG-based.
+    // I guess it's sensible to adapt VSG's allocator with a PhysX-specific affinity as the default PhysX allocator doesn't have any anti-fragmentation measures.
     physx::PxDefaultAllocator physXAllocator;
 
     class ErrorCallback : public physx::PxErrorCallback
@@ -21,7 +23,43 @@ namespace
     public:
         ~ErrorCallback() override = default;
 
-        void reportError(physx::PxErrorCode::Enum code, const char* message, const char* file, int line) override{};
+        void reportError(physx::PxErrorCode::Enum code, const char* message, const char* file, int line) override
+        {
+            vsg::Logger::Level level = vsg::Logger::LOGGER_DEBUG;
+            switch (code)
+            {
+            case physx::PxErrorCode::eNO_ERROR:
+                level = vsg::Logger::LOGGER_DEBUG;
+                break;
+            case physx::PxErrorCode::eDEBUG_INFO:
+                level = vsg::Logger::LOGGER_INFO;
+                break;
+            case physx::PxErrorCode::eDEBUG_WARNING:
+                level = vsg::Logger::LOGGER_WARN;
+                break;
+            case physx::PxErrorCode::eINVALID_PARAMETER:
+                level = vsg::Logger::LOGGER_ERROR;
+                break;
+            case physx::PxErrorCode::eINVALID_OPERATION:
+                level = vsg::Logger::LOGGER_ERROR;
+                break;
+            case physx::PxErrorCode::eOUT_OF_MEMORY:
+                level = vsg::Logger::LOGGER_ERROR;
+                break;
+            case physx::PxErrorCode::eINTERNAL_ERROR:
+                level = vsg::Logger::LOGGER_ERROR;
+                break;
+            case physx::PxErrorCode::eABORT:
+                level = vsg::Logger::LOGGER_FATAL;
+                break;
+            case physx::PxErrorCode::ePERF_WARNING:
+                level = vsg::Logger::LOGGER_WARN;
+                break;
+            default:
+                break;
+            }
+            vsg::log(level, "PhysX ", file, " line ", line, ": ", message);
+        };
     };
 
     ErrorCallback errorCallback;
@@ -48,11 +86,9 @@ std::unique_ptr<vsgPhysX::Engine>& vsgPhysX::Engine::instanceInternal()
 vsgPhysX::Engine::Engine(const physx::PxTolerancesScale& tolerancesScale) :
     _foundation(PxCreateFoundation(PX_PHYSICS_VERSION, physXAllocator, errorCallback)), _physics(PxCreatePhysics(PX_PHYSICS_VERSION, *_foundation, tolerancesScale, false, nullptr, nullptr))
 {
-    std::cout << "creating Engine" << std::endl;
     defaultMaterial = _physics->createMaterial(0.5f, 0.5f, 0.5f);
 }
 
 vsgPhysX::Engine::~Engine()
 {
-    std::cout << "destroying Engine" << std::endl;
 }
