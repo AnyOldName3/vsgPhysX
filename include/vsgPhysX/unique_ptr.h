1502e2c5e6bc530e81412b317dea1dc97f920f6b
AnyOldName3 <krizdjali@gmail.com>
2024-05-10 19:15:26 +0100

    unique_ptr_static_cast

--- include/vsgPhysX/unique_ptr.h
+++ include/vsgPhysX/unique_ptr.h
@@ -16,4 +16,20 @@ namespace vsgPhysX
 
     template<class T>
     using unique_ptr = std::unique_ptr<T, pxRelease<T>>;
+
+    template<class T, class U>
+    unique_ptr<T> unique_ptr_static_cast(unique_ptr<U>& rhs)
+    {
+        return unique_ptr<T>(static_cast<T*>(rhs.release()));
+    }
+
+    /* Currently disabled as PhysX defaults to RTTI off and I've not figured out how to test it
+    template<class T, class U>
+    unique_ptr<T> unique_ptr_dynamic_cast(unique_ptr<U>& rhs)
+    {
+        if (dynamic_cast<T*>(rhs.get()))
+            return unique_ptr_static_cast<T>(rhs);
+        else
+            return nullptr;
+    }*/
 } // namespace vsgPhysX
